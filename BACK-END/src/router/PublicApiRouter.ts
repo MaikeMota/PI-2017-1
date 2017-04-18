b23d8666caa22d71b9b82b10dc806a338470e90e
Maike Mota <maike_henrique@hotmail.com>
2017-04-18 17:01:07 -0300

    Added EchoRouter to PublicApiRouter

--- BACK-END/src/router/PublicApiRouter.ts
+++ BACK-END/src/router/PublicApiRouter.ts
@@ -2,12 +2,14 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { BaseRouter } from './BaseRouter';
 
 import { TokenRouter } from './TokenRouter';
+import { EchoRouter } from './EchoRouter';
 import { RestrictedApiRouter } from './RestrictedApiRouter';
 
 export class PublicApiRouter extends BaseRouter {
 
     protected configureRouter(): void {
         this.register('token', TokenRouter);
+        this.register('echo', EchoRouter);
         this.register('restricted', RestrictedApiRouter);
     }
 }
