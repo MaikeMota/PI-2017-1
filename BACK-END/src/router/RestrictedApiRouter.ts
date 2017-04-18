dd0eac0391b9b5be5dd2b42920cda7a1cb254f30
Maike Mota <maike_henrique@hotmail.com>
2017-04-18 15:46:11 -0300

    Added RestrictedApiRouter

---
+++ BACK-END/src/router/RestrictedApiRouter.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { BaseRouter } from './BaseRouter';
+
+
+export class RestrictedApiRouter extends BaseRouter {
+
+    protected configureRouter(): void {
+        //todo
+    }
+}
