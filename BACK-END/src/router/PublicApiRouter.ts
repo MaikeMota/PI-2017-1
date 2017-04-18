ae25b863181349ce1a76ed596212d6fe50017a81
Maike Mota <maike_henrique@hotmail.com>
2017-04-18 15:48:28 -0300

    Register RestrictedApiRouter as a subrouter of PublicApiRouter

--- BACK-END/src/router/PublicApiRouter.ts
+++ BACK-END/src/router/PublicApiRouter.ts
@@ -2,11 +2,12 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { BaseRouter } from './BaseRouter';
 
 import { TokenRouter } from './TokenRouter';
-
+import { RestrictedApiRouter } from './RestrictedApiRouter';
 
 export class PublicApiRouter extends BaseRouter {
 
     protected configureRouter(): void {
         this.register('token', TokenRouter);
+        this.register('restricted', RestrictedApiRouter);
     }
 }
