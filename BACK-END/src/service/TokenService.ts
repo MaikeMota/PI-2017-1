fbb54816c863b9876d6f3345af38e11ba040c029
Maike Mota <maike_henrique@hotmail.com>
2017-04-19 13:46:08 -0300

    Token generation using RSA keys

--- BACK-END/src/service/TokenService.ts
+++ BACK-END/src/service/TokenService.ts
@@ -1,16 +1,33 @@
 import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs'
+
+import { User } from '../model/User';
 
 export class TokenService {
 
+    private static _privateKey;
+
+    private static get privateKey(): any {
+        if (!TokenService._privateKey) {
+            TokenService._privateKey = fs.readFileSync('./resources/privateKey.key');
+        }
+        return TokenService._privateKey;
+    }
+    private static _publicKey;
+
+    private static get publicKey(): any {
+        if (!TokenService._publicKey) {
+            TokenService._publicKey = fs.readFileSync('./resources/publicKey.pem');
+        }
+        return TokenService._publicKey;
+    }
+
     public static signToken(): string {
-        return jwt.sign("Usuário", "SECRET");
+        return jwt.sign('Usuário', TokenService.privateKey, { algorithm: 'RS256' });
     }
 
     public static renewToken(oldToken: string): string {
-        jwt.decode(oldToken);
-
         //VALIDATE
-
         return TokenService.signToken();
 
     }
