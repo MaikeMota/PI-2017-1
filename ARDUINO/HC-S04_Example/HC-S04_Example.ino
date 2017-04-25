b72481d395da4c8a8d47ef8a7f18e8ef34ecf73e
Maike Mota <maike_henrique@hotmail.com>
2017-04-25 00:44:23 -0300

    Added HC-S04 Ultrasonic Sensor Example
    Example of volume reading with Ultrasonic HC-S04.

---
+++ ARDUINO/HC-S04_Example/HC-S04_Example.ino
@@ -0,0 +1,55 @@
+//Programa: Conectando Sensor Ultrassonico HC-SR04 ao Arduino
+//Autor: FILIPEFLOP
+ 
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+
+#define PI 3.14159
+#define R 10
+#define H_MAX 25.2
+#define H_MIN 22.8
+
+const int SAMPLES = 10;
+ 
+//Define os pinos para o trigger e echo
+#define pino_trigger 4
+#define pino_echo 5
+ 
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+float H_ATUAL = 0; 
+ 
+void setup()
+{
+  Serial.begin(9600);
+  //Serial.println("Lendo dados do sensor...");
+}
+
+int estado_atual = -1;
+void loop()
+{
+  float volume = 0,  cmMsec = 0;
+  long microsec = 0;
+  microsec = ultrasonic.timing();
+  cmMsec += ultrasonic.convert(microsec, Ultrasonic::CM);
+  H_ATUAL = H_MAX - cmMsec;
+  volume = PI * pow(R, 2) * H_ATUAL;
+  volume /= 1000;
+
+  if(volume < 1 && estado_atual != 0) {
+    Serial.println("ABAIXO DO MINIMO");
+    estado_atual = 0;
+  }else if(volume <= 3.5 && estado_atual != 1){
+    Serial.println("ABAIXO DO MEDIO");
+    estado_atual = 1;
+  }else if(volume > 3.5 && volume < 6 && estado_atual != 2){
+    Serial.println("ACIMA DO MEDIO");
+    estado_atual = 2;
+  }else if(volume >= 6 && estado_atual != 3){
+    Serial.println("MAXIMO");
+    estado_atual = 3;
+  }
+  Serial.print("V: "); Serial.println(volume);
+  delay(500);
+}
