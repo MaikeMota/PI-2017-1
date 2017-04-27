93ea807eb288ffa208833af45fcbc868d618f7c0
Reginaldo <reginaldocmrg@hotmail.com>
2017-04-27 02:24:43 -0300

    Started adding validation to the router

--- BACK-END/src/router/RestrictedApiRouter.ts
+++ BACK-END/src/router/RestrictedApiRouter.ts
@@ -1,10 +1,24 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, RequestHandler, Response, NextFunction } from 'express';
 import { BaseRouter } from './BaseRouter';
+import { CalculatorRouter } from './CalculatorRouter';
+
+import { RTKException } from '../api/rethink/core';
+import { ResponseUtil } from '../api/rethink/util';
 
 
 export class RestrictedApiRouter extends BaseRouter {
 
     protected configureRouter(): void {
-        //todo
+        this.register('calculator', CalculatorRouter);
+    }
+
+    protected configureMiddleware(): void {
+        this.router.use((request: Request, response: Response, next: NextFunction) => {
+            if (!request.header('authorization')) {
+                ResponseUtil.forbidden(response, new RTKException("Missing Token Authorzation", -1));
+            } else {
+                next();
+            }
+        });
     }
 }
