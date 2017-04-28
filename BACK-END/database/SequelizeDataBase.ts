94d103d23d3c816a582af253cf1600b90ac604ea
Maike Mota <maike_henrique@hotmail.com>
2017-04-28 17:21:22 -0300

    New Sequelize initialization Model

---
+++ BACK-END/database/SequelizeDataBase.ts
@@ -0,0 +1,88 @@
+import { readdirSync } from 'fs';
+import * as path from 'path';
+
+import * as SequelizeStatic from 'sequelize';
+import { Sequelize } from "sequelize";
+
+import { UnregisteredModelException } from '../src/api/rethink/core/exception'
+
+import { User, UserAttributes } from '../src/model/interface';
+import { Entity, EntityAttributes } from '../src/model/interface';
+
+export class SequelizeDataBase {
+
+    public static instance: SequelizeDataBase;
+
+    private _sequelize: Sequelize;
+    private _models: SequelizeModels = new SequelizeModels();
+
+    public constructor() {
+    }
+
+    private registerDefaultModels(): void {
+
+        readdirSync('./src/model/sequelize').filter((file: string) => {
+            return (file !== path.basename(module.filename) && (file !== "interface"));
+        }).forEach((file: string) => {
+            let model: SequelizeStatic.Model<Entity<EntityAttributes>, EntityAttributes> =
+                this.sequelize.import<Entity<EntityAttributes>, EntityAttributes>(path.join('../src/model/sequelize', file));
+            this._models[(model as any).name] = model;
+        });
+
+        /* Object.keys(this.models).forEach((modelName: string) => {
+             if (typeof this.models[modelName].associate === "function") {
+                 this.models[modelName].associate(this.models);
+             }
+         })*/
+
+        Object.keys(this._models).forEach((modelName) => {
+            this._models[modelName].sync({ force: false });
+        });
+
+    }
+
+    /*    
+    return SequelizeInstance.UserModel.create({
+                username: 'system',
+                password: 'system1234SYSTEM!@#$'
+            });
+     */
+
+    public static initializeDatabase() {
+        let sequelizeDatabase = new SequelizeDataBase();
+        sequelizeDatabase._sequelize = new SequelizeStatic('database', 'system', 'master1234MASTER!@#$', { // TODO Migrate to a config file
+            host: 'localhost',
+            dialect: 'sqlite',
+            pool: {
+                max: 5,
+                min: 1,
+                idle: 10000
+            },
+            // SQLite only
+            storage: './database/storage.db'
+        });
+
+        sequelizeDatabase.registerDefaultModels();
+
+        sequelizeDB = sequelizeDatabase;
+    }
+
+    public getModel(modelName: string): SequelizeStatic.Model<Entity<EntityAttributes>, EntityAttributes> {
+        let model = this._models[modelName];
+        if (model) {
+            return model;
+        } else {
+            throw new UnregisteredModelException(`The 'model' ${modelName} was not registered.`, -1);
+        }
+    }
+
+    public get sequelize(): Sequelize {
+        return this._sequelize;
+    }
+}
+
+class SequelizeModels {
+    [key: string]: SequelizeStatic.Model<Entity<EntityAttributes>, EntityAttributes>;
+}
+
+export var sequelizeDB: SequelizeDataBase;
