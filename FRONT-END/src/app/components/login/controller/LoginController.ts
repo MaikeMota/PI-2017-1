446da4aab633c3b647c66bc6cf0ed8ba3b20ee02
Reginaldo <reginaldocmrg@hotmail.com>
2017-05-06 16:59:19 -0300

    Almost finished login

--- FRONT-END/src/app/components/login/controller/LoginController.ts
+++ FRONT-END/src/app/components/login/controller/LoginController.ts
@@ -1,8 +1,10 @@
 import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Controller } from '../../../shared/Controller';
 import { Definitions } from '../../../shared/Definitions';
 import { User } from '../../../entities/User';
+import { UserInfoService } from '../../../services/UserInfoService';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -10,13 +12,14 @@ export class LoginController extends Controller<User> {
 
     public static readonly ENDPOINT: string = "token";
 
-    constructor(private http: Http) {
+    constructor(private http: Http, private router: Router, private userInfoService: UserInfoService) {
         super();
     }
     
     public login(): void {
         this.http.post(Definitions.SERVER_ADDRESS+"/"+LoginController.ENDPOINT, this.entity).toPromise().then(response => {
-            console.log(response);
+            this.userInfoService.token = response.json().token;
+            this.router.navigate(['dashboard']);
         }).catch(error => {
             console.error(error);
         });
--- FRONT-END/src/app/services/UserInfoService.ts
+++ FRONT-END/src/app/services/UserInfoService.ts
@@ -2,5 +2,25 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UserInfoService {
-    
+    _token: string;
+    private static readonly CLASS_NAME: string = "UserInfoService";
+
+    public get token(): string {
+        return this._token;
+    }
+
+    public set token(token: string) {
+        this._token = token;
+        this.save();
+    }
+
+    public save(): void {
+        let info: string = JSON.stringify(this);
+        sessionStorage.setItem(UserInfoService.CLASS_NAME, info);
+    }
+
+    public recover(): void {
+        let info: any = JSON.parse(sessionStorage.getItem(UserInfoService.CLASS_NAME));
+        this._token = info.token;
+    }
 }
