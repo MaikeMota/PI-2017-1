c6d24106f3819956df69e48493d6a9e37757ce6b
Reginaldo <reginaldocmrg@hotmail.com>
2017-05-07 03:54:02 -0300

    Started html structure to show the devices as cards

--- FRONT-END/src/app/components/dashboard/DashboardComponent.ts
+++ FRONT-END/src/app/components/dashboard/DashboardComponent.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 	title = 'Controle de Cisternas Híbridas';
+
+	public devices = [{
+		description: "This is the first device of the array to test the interface with a very large number of elements."
+	}, {
+		description: "This is the second device of the array to test the interface with a very large number of elements."
+	}, {
+		description: "This is the third device of the array to test the interface with a very large number of elements."
+	}, {
+		description: "This is the fourth device of the array to test the interface with a very large number of elements."
+	}];
 }
