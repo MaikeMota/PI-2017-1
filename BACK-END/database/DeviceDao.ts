81dda7702e21c7518c2725f6786af370756b5879
Maike Mota <maike_henrique@hotmail.com>
2017-05-08 01:34:53 -0300

    Fixes byDeviceKey method

--- BACK-END/database/DeviceDao.ts
+++ BACK-END/database/DeviceDao.ts
@@ -1,12 +1,21 @@
 import { GenericDao } from "./GenericDao";
 import { Device, DeviceInstance } from "../src/model/interface";
+import { EntityNotFoundException } from "../../RETHINK/core/exception/index";
 
 export class DeviceDao extends GenericDao<DeviceInstance, Device> {
 
-    public byDeviceKey(deviceKey: string): Promise<Device> {
+    public byDeviceKey(device_key: string): Promise<Device> {
         return new Promise<Device>((resolve, reject) => {
-            this.getModelForEntity(Device).findByPrimary(deviceKey).then((result) => {
-                resolve(result.dataValues);
+            this.getModelForEntity(Device).findOne({
+                where: {
+                    device_key: device_key
+                }
+            }).then((result) => {
+                if (result) {
+                    resolve(result.dataValues);
+                } else {
+                    throw new EntityNotFoundException(`Cannot find a device where device_key=${device_key}`, -1);
+                }
             }).catch(error => {
                 reject(error);
             });
