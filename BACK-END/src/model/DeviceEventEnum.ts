7f29b78b6b6cb632d6d6f01cd484566e6818b456
Maike Mota <maike_henrique@hotmail.com>
2017-05-07 22:09:00 -0300

    Removes unnecessary events

--- BACK-END/src/model/DeviceEventEnum.ts
+++ BACK-END/src/model/DeviceEventEnum.ts
@@ -6,9 +6,6 @@ export class DeviceDataEventEnum extends Enum {
         super(name);
     }
 
-    public static readonly MIN_LEVEL_REACHED: DeviceDataEventEnum = new DeviceDataEventEnum('MIN_LEVEL_REACHED');
-    public static readonly MED_LEVEL_REACHED: DeviceDataEventEnum = new DeviceDataEventEnum('MED_LEVEL_REACHED');
-    public static readonly MAX_LEVEL_REACHED: DeviceDataEventEnum = new DeviceDataEventEnum('MAX_LEVEL_REACHED');
     public static readonly WATER_INLET_FLUX_STARTED: DeviceDataEventEnum = new DeviceDataEventEnum('WATER_INLET_FLUX_STARTED');
     public static readonly WATER_INLET_FLUX_STOPED: DeviceDataEventEnum = new DeviceDataEventEnum('WATER_INLET_FLUX_STOPED');
     public static readonly WATER_OUT_FLUX_STARTED: DeviceDataEventEnum = new DeviceDataEventEnum('WATER_OUT_FLUX_STARTED');
@@ -18,9 +15,6 @@ export class DeviceDataEventEnum extends Enum {
 
     public static value(): DeviceDataEventEnum[] {
         return [
-            DeviceDataEventEnum.MIN_LEVEL_REACHED,
-            DeviceDataEventEnum.MED_LEVEL_REACHED,
-            DeviceDataEventEnum.MAX_LEVEL_REACHED,
             DeviceDataEventEnum.WATER_INLET_FLUX_STARTED,
             DeviceDataEventEnum.WATER_INLET_FLUX_STOPED,
             DeviceDataEventEnum.WATER_OUT_FLUX_STARTED,
