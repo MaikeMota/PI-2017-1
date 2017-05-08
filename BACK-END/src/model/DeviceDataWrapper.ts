b71ec4109674c4e70da3f4edfcdaf53cad7bb17a
Maike Mota <maike_henrique@hotmail.com>
2017-05-07 22:08:37 -0300

    Wrapper to receive data from arduino and parse to Model

--- BACK-END/src/model/DeviceDataWrapper.ts
+++ BACK-END/src/model/DeviceDataWrapper.ts
@@ -1,6 +1,10 @@
-import { DeviceData } from "./interface";
+import { DeviceData, Device } from "./interface";
+import { DeviceDataEvent } from "./interface/DeviceDataEvent";
+import { DeviceDataEventEnum } from "./DeviceEventEnum";
 
 export class DeviceDataWrapper {
+    device_key: string;
+    device: Device
     waterLevel: number
     waterInletFlux: number
     waterOutFlux: number
@@ -8,7 +12,18 @@ export class DeviceDataWrapper {
 
     public toDeviceDataEntity(): DeviceData {
         let deviceData = new DeviceData();
+        deviceData.device = this.device
+        deviceData.water_level = this.waterLevel;
+        deviceData.water_inlet_flux = this.waterInletFlux;
+        deviceData.water_out_flux = this.waterOutFlux;
 
+        deviceData.events = [];
+        for (let e of this.events) {
+            let event = new DeviceDataEvent();
+            event.deviceData = deviceData;
+            event.event = DeviceDataEventEnum.fromIndex(e);
+            deviceData.events.push(event);
+        }
         return deviceData;
     }
 }
