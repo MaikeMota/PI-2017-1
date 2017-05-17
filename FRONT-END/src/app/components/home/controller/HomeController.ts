2bbd8ba31c84f97556842db908973f2b9f1635cb
Maike Mota <maike_henrique@hotmail.com>
2017-05-17 00:54:08 -0300

    Start listening to socket when  login and stop on logout

--- FRONT-END/src/app/components/home/controller/HomeController.ts
+++ FRONT-END/src/app/components/home/controller/HomeController.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AbstractController } from '../../../../../../RETHINK/core';
 import { UserInfoService } from '../../../services/UserInfoService';
+import { SocketService, DeviceStorageService } from "../../../services";
 
 @Injectable()
 export class HomeController extends AbstractController<any> {
 
-    constructor(private userInfoService: UserInfoService, private router: Router) {
+    constructor(private socketService: SocketService, private deviceStorageService: DeviceStorageService, private userInfoService: UserInfoService, private router: Router) {
         super();
+        this.socketService.startListening();
     }
 
     clean() {
@@ -16,6 +18,8 @@ export class HomeController extends AbstractController<any> {
 
     public logout() {
         this.userInfoService.token = undefined;
+        this.socketService.stopListening();
+        this.deviceStorageService.clear();
         this.router.navigate(["login"]);
     }
 }
