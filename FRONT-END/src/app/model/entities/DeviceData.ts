f1fa4b2e10c9d3337f9b5dd351141d056b411ccd
Maike Mota <maike_henrique@hotmail.com>
2017-05-17 13:52:29 -0300

    Displays 'createdAt' property

--- FRONT-END/src/app/model/entities/DeviceData.ts
+++ FRONT-END/src/app/model/entities/DeviceData.ts
@@ -1,5 +1,5 @@
 import { Device } from './';
-import { Enum, Entity } from '../../../../../RETHINK/core';
+import { Enum, Entity, typed } from '../../../../../RETHINK/core';
 import { DeviceDataEvent } from "./DeviceDataEvent";
 
 export class DeviceData extends Entity {
@@ -7,11 +7,13 @@ export class DeviceData extends Entity {
     constructor() {
         super();
     }
-    
+
     public water_level: number;
     public water_inlet_flux: number;
     public water_out_flux: number;
     public device_id: string;
+    @Reflect.metadata('design:type', Date)
+    public createdAt: Date;
     @Reflect.metadata('design:arrayType', DeviceDataEvent)
     public events: DeviceDataEvent[] = [];
 }
